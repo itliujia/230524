0c4c5551fc25a29124672adc44dc41fc83d98250
itliujia <2264284303@qq.com>
2023-05-30 14:12:12 +0800

    数据库结构变更

--- seatReservate/library.sql
+++ seatReservate/library.sql
@@ -1,13 +1,32 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : library
+ Source Server Type    : MySQL
+ Source Server Version : 50726
+ Source Host           : localhost:3306
+ Source Schema         : library
+
+ Target Server Type    : MySQL
+ Target Server Version : 50726
+ File Encoding         : 65001
+
+ Date: 30/05/2023 14:11:35
+*/
+
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
+-- ----------------------------
+-- Table structure for admin
+-- ----------------------------
 DROP TABLE IF EXISTS `admin`;
 CREATE TABLE `admin`  (
   `id` int(11) NOT NULL,
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of admin
@@ -19,13 +38,13 @@ INSERT INTO `admin` VALUES (1, 'admin', '123456');
 -- ----------------------------
 DROP TABLE IF EXISTS `bookings`;
 CREATE TABLE `bookings`  (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `seat_id` int(11) NULL DEFAULT NULL,
   `user_id` int(11) NULL DEFAULT NULL,
   `start_time` datetime NULL DEFAULT NULL,
   `end_time` datetime NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Fixed;
+) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = FIXED;
 
 -- ----------------------------
 -- Records of bookings
@@ -36,16 +55,23 @@ CREATE TABLE `bookings`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `seats`;
 CREATE TABLE `seats`  (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `description` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
-  `status` int(11) NULL DEFAULT NULL,
+  `status` int(11) NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = MyISAM AUTO_INCREMENT = 9 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of seats
 -- ----------------------------
+INSERT INTO `seats` VALUES (1, 'A1', '这是A1座位', 0);
+INSERT INTO `seats` VALUES (2, 'A2', '这是A2座位', 1);
+INSERT INTO `seats` VALUES (3, 'B1', '这是B1座位', 0);
+INSERT INTO `seats` VALUES (4, 'B2', '这是B2座位', 0);
+INSERT INTO `seats` VALUES (5, 'C1', '这是C1座位', 1);
+INSERT INTO `seats` VALUES (6, 'C2', '这是C2座位', 0);
+INSERT INTO `seats` VALUES (8, 'A3', '这是A3座位', 0);
 
 -- ----------------------------
 -- Table structure for users
@@ -57,7 +83,7 @@ CREATE TABLE `users`  (
   `stuno` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = MyISAM AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = MyISAM AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of users
